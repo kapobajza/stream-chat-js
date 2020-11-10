490108d574e0255243ad66bc1106ea2eadf421c9
ruudniew <ruudniew@protonmail.com>
2020-11-10 22:32:48 +0100

    Stream local test host

--- test/integration/connection.js
+++ test/integration/connection.js
@@ -9,7 +9,7 @@ import sinon from 'sinon';
 import { TokenManager } from '../../src/token_manager';
 
 const wsBaseURL = process.env.STREAM_LOCAL_TEST_RUN
-	? 'ws://localhost:3030'
+	? 'ws://' + process.env.STREAM_LOCAL_TEST_HOST || 'ws://localhost:3030'
 	: 'wss://chat-us-east-1.stream-io-api.com';
 
 const tokenProvider = () =>
